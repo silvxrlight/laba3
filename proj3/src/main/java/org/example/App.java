7220f5943c61c11e097ee45f94dfda24b40cb25b
Gleb Popov <72411566+silvxrlight@users.noreply.github.com>
2025-08-27 19:43:35 +0300

    Delete proj3 directory

--- proj3/src/main/java/org/example/App.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.example.ui.MainFrame;
-
-public class App {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
-    }
-}
